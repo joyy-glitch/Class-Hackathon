7f381d9f4aa56d742cf3066f245694ceded5e70a
Joy Addy <joyaddy267@gmail.com>
2025-09-25 12:20:36 +0000

    beginning

--- src/Question7.java
+++ src/Question7.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 
 public class Question7 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter two characters: ");
+    System.out.println("Enter two characters: ");
         String word = input.nextLine();
 
 
@@ -60,6 +60,73 @@ public class Question7 {
                 case '4':
                     System.out.println("IT Senior");
                     break;
+            }
+        }
+
+ */
+
+        //Question 3
+
+import java.util.Scanner;
+
+        public class Class_trial {
+            public static void main(String args[])
+            {
+                Scanner input = new Scanner(System.in);
+
+
+                System.out.print("Enter a letter:");
+                String letter =  input.nextLine();
+
+
+                switch(letter){
+                    case"a","b","c":
+                        System.out.println("1");
+                        break;
+
+                    case"d","e","f":
+                        System.out.println("2");
+                        break;
+
+                    case"g","h","i":
+                        System.out.println("3");
+                        break;
+
+                    case"j","k","l":
+                        System.out.println("4");
+                        break;
+
+                    case"m","n","o":
+                        System.out.println("5");
+                        break;
+
+                    case"p","q","r","s":
+                        System.out.println("6");
+
+                    case"t","u","v":
+                        System.out.println("7");
+                        break;
+
+                    case"w","x","y","z":
+                        System.out.println("8");
+
+                        //Question 5
+
+                        byte s = input.nextByte() ;
+
+
+
+
+
+
+                }
+
+
+
+
+
+
+
             }
         }
 
